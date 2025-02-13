76cd9e902f365a88dc002764517132bd1cf6c4f7
Tgaza <tagazawa55@gmail.com>
2025-02-13 15:44:10 -0700

    started to work on inputgen and verifier

--- Verifier.java
+++ Verifier.java
@@ -1,4 +1,12 @@
+import java.io.FileReader;
 
 public class Verifier {
+    private int totalRows;
+    private int totalCols;
+    private int[] rowValues;
+    private int[] colValues;
 
+    public Verifier() {
+
+    }
 }
