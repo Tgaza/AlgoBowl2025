90d4df666039d6be2e345d98a1c9e64005e3c927
t-dowd <dowdthomasj@gmail.com>
2025-02-12 19:26:33 -0700

    Adjusted Files to better match Eclipse IDE

---
+++ AlgoProject/src/lab/PrimaryTesting.java
@@ -0,0 +1,24 @@
+/**
+ * Testing Area for the algorithm
+ * "{InsertTeamName}"
+ * @author Ty Gazaway
+ * @author John Silva 
+ * @author Thomas Dowd
+ * 
+ */
+package lab;
+
+/**
+ * 
+ */
+public class PrimaryTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		TODO: Add Testing
+//		TODO: Link to Junit
+	}
+
+}
