ed07dc3ca42c6f411577c8a5ab257f36669835b6
Johnathon <jesilva@mines.edu>
2025-02-16 17:16:04 -0700

    First Iteration of Input Generation

--- AlgoProject/src/main/InputGenerator.java
+++ AlgoProject/src/main/InputGenerator.java
@@ -8,9 +8,102 @@
  */
 package main;
 
+import java.util.Random;
+
 /**
  * 
  */
 public class InputGenerator {
-
+	private int rows = 15;
+	private int columns = 35;
+	private int[] rowTents = new int[rows];
+	private int[] columnTents = new int[columns];
+	private char[][] gameGrid = new char[rows][columns];
+	private int[] treeRowCount = new int[rows];
+	private int[] treeColCount = new int[columns];
+	
+	private static final double LAMBDA = 0.0005;
+	
+	public Random rand = new Random();
+	
+	public static boolean shouldPlaceTree(int n, int m) {
+        int numTiles = n * m;
+        double probability = 1 - Math.exp(-LAMBDA * numTiles);
+        Random random = new Random();
+        return random.nextDouble() < probability; // Returns true if a tree should be placed
+    }
+	
+	public InputGenerator() {
+		//generate spot for a tree or open space for the entire graph
+		for(int row = 0; row < rows; row++) {
+			for(int column = 0; column < columns; column++) {
+				//kind of the heuristic for determining the density of trees on the graph
+				boolean place = shouldPlaceTree(rows, columns);
+				if(place) {
+					gameGrid[row][column] = 'T';
+					treeRowCount[row]++;
+					treeColCount[column]++;
+				} else {
+					gameGrid[row][column] = '.';
+				}
+			}
+			System.out.println();
+		}
+		
+		//check and make sure that no trees are completed locked in by other trees as that forces a violation
+		for(int row = 0; row < rows; row++) {
+			for(int col = 0; col < columns; col++) {
+				int blockCount = 0;
+				if(row-1 > 0 && gameGrid[row][col] == 'T') {
+					blockCount++;
+				} else if(row+1 < rows && gameGrid[row][col] == 'T') {
+					blockCount++;
+				} else if(col-1 > 0 && gameGrid[row][col] == 'T') {
+					blockCount++;
+				} else if(col+1 < rows && gameGrid[row][col] == 'T') {
+					blockCount++;
+				}
+				
+				if(blockCount == 4) {
+					treeRowCount[row]--;
+					treeColCount[col]--;
+					gameGrid[row][col] = '.';
+				}
+			}
+		}
+		
+		//print out the rows and columns of the gameGrid
+		System.out.println(rows + " " + columns);
+		
+		//generate number of tents for the row and columns
+		for(int i = 0; i < rows; i++) {
+			int randomInt = rand.nextInt(rows - treeRowCount[i]);
+			rowTents[i] = randomInt;
+			System.out.print(randomInt + " ");
+		}
+		
+		//print out a newline to separate the rows and columns
+		System.out.println();
+		
+		for(int i = 0; i < columns; i++) {
+			int randomInt = rand.nextInt(rows - treeColCount[i]);
+			columnTents[i] = randomInt;
+			System.out.print(randomInt + " ");
+		}
+		
+		System.out.println();
+		
+		//print out generated grid
+		for(int row = 0; row < rows; row++) {
+			for(int column = 0; column < columns; column++) {
+				System.out.print(gameGrid[row][column] + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new InputGenerator();
+	}
 }
