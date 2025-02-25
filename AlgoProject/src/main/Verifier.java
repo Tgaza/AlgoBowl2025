b07a251b776628f10320384680f0b9ffbef826d4
t-dowd <dowdthomasj@gmail.com>
2025-02-24 18:14:20 -0700

    Created TODO's for verifier

--- AlgoProject/src/main/Verifier.java
+++ AlgoProject/src/main/Verifier.java
@@ -8,8 +8,67 @@
 package main;
 
 /**
- * -TODO: Verifier
+ * -TODO: Create new Offical Folder to pull solutions/Inputfiles from
+ * -TODO: Ensure one indexed output !!!
+ * 
+ * 
+ * -TODO: Incorect num of violations <- INVALID
+ * -TODO: Tent Superposition (Tent cannot overlap with existing entity) <- INVALID
+ * -TODO: Cant Fall off edge of world <- INVALID
+ * -TODO: Tent cannot be paired with non-tree entity <-INVALID
+ * -TODO: File must have proper formatting + No missing/corrupt data <- INVALID
+ * 
+ * 
+ * -TODO: Cant be an unpaired entity <- Violation
+ * -TODO: Multiple adjenecies != multiple violations (Specifially with Tents) <- Violation
+ * -TODO: A row or column which has too many or too few tents causes multiple violations: one violation for each tent to many or too few<- Violation 
+ * 
+ * When complete, "todo" -> "fixme"
  */
 public class Verifier {
-
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+	public Verifier() {
+		super();
+	}
+	
+	// Big Guys
+	
+	public void checkInvalid() {
+		
+	}
+	
+	
+	public void countViolations() {
+		
+	}
+	
+	// Little Guys
+	
+	public void readInput() {
+		
+	}
+	
+	public void checkFormatting() {
+		
+	}
+	
+	/**
+	 * Update cells into a grid into 2d grid.
+	 */
+	public void placeTents() {
+		
+	}
+	
+	public void checkPairs() {
+		
+	}
+	
+	// Etc
+	
 }
